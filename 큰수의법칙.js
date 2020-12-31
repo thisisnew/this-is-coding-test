60fe682da741c78d45bbab1b9c0a89921be83550
thisisnew <thisisnew@naver.com>
2020-12-31 22:37:05 +0900

    큰수의법칙 공부

---
+++ 큰수의법칙.js
@@ -0,0 +1,26 @@
+const arr = [2,4,5,4,6];
+
+const m = 8;
+const k = 3;
+
+let first = arr[0];
+let second;
+
+arr.forEach(item => {
+    if( item > first ){
+        second = first;
+        first = item;
+    }
+});
+
+let total = 0;
+
+for( let i = 1; i <=m; i++ ){
+    if( i % k === 0 ){
+        total += second;
+    } else {
+        total += first;
+    }
+}
+
+console.log( '합:', total );

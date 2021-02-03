07b31d3762126dcf60b9366ebf5034716acf7ff4
thisisnew <thisisnew@naver.com>
2021-02-03 15:52:23 +0900

    특정거리의 도시 찾기

---
+++ study-algorithm/src/ch13/ex15.java
@@ -0,0 +1,67 @@
+package ch13;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class ex15 {
+	 
+	static int n, m, k, x;
+	static List<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+	
+	static int[] distance = new int[300001];
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		m = scan.nextInt();
+		k = scan.nextInt();
+		x = scan.nextInt();
+		scan.nextLine();
+		
+		for(int i=0; i<=n; i++) {
+			list.add(new ArrayList<Integer>());
+			distance[i] = -1;
+		}
+		
+		for(int i=0; i < m; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			list.get(a).add(b);
+		}
+		
+		distance[x] = 0;
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.offer(x);
+		
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			
+			for(int i=0; i<list.get(now).size(); i++) {
+				int next = list.get(now).get(i);
+				
+				if(distance[next] == -1) {
+					distance[next] = distance[now] + 1;
+					q.offer(next);
+				}
+				
+			}
+		}
+		
+		boolean check = false;
+		
+		for(int i=1; i<=n; i++) {
+			if(distance[i] == k) {
+				System.out.println(i);
+				check = true;
+			}
+		}
+		
+		if(!check) {
+			System.out.println(-1);
+		}
+	}
+}

12304def37bc1144311c883af85cd60a88424788
thisisnew <thisisnew@naver.com>
2021-02-09 19:38:05 +0900

    문자열 처리 공부

--- study-algorithm/src/baekjoon/string/Ex2438.java
+++ study-algorithm/src/baekjoon/string/Ex2438.java
@@ -11,9 +11,11 @@ public class Ex2438 {
 		n = scanner.nextInt();
 		
 		for(int i=1; i<=n; i++) {
+			
 			for(int j=0; j<i; j++) {
 				System.out.print("*");
 			}
+			
 			System.out.println();
 		}
 		

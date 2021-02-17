413db6210562c6d3824efc678880774263a04d39
thisisnew <thisisnew@naver.com>
2021-02-17 11:56:40 +0900

    bfs를 활용한 토마토 문제 해결

--- study-algorithm/src/baekjoon/bfsdfs/Ex7576.java
+++ study-algorithm/src/baekjoon/bfsdfs/Ex7576.java
@@ -1,103 +1,108 @@
 package baekjoon.bfsdfs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-class Nodes7576 {
+class Node7576 {
 	private int a, b, cnt;
 	
-	public Nodes7576(int a, int b, int cnt) {
+	public Node7576(int a, int b, int cnt) {
 		this.a = a;
 		this.b = b;
 		this.cnt = cnt;
 	}
-	
+
 	public int getA() {
 		return a;
 	}
-	
+
 	public int getB() {
 		return b;
 	}
-	
+
 	public int getCnt() {
 		return cnt;
 	}
 }
 
+
 public class Ex7576 {
 	
-	static int n, m;
-	static int[][] array;
+	static int m, n;
+	static int[][] graph;
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
 	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		
+		graph = new int[n][m];
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+//		for(int i=0; i<n; i++) {
+//			System.out.println();
+//			for(int j=0; j<m; j++) {
+//				System.out.print(graph[i][j] + " ");
+//			}
+//		}
+		
+		System.out.println(bfs());
+	}
+	
 	static int bfs() {
-		Queue<Nodes7576> q = new LinkedList<Nodes7576>();
+		Queue<Node7576> q = new LinkedList<Node7576>();
 		int cnt = 0;
 		
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<m; j++) {
-				if(array[i][j] == 1) {
-					q.offer(new Nodes7576(i, j, 0));
+				if(graph[i][j] == 1) {
+					q.offer(new Node7576(i, j, 0));
 				}
 			}
 		}
 		
 		while(!q.isEmpty()) {
-			Nodes7576 node = q.poll();
+			Node7576 node = q.poll();
 			cnt = node.getCnt();
 			
 			for(int i=0; i<4; i++) {
-				int nx = dx[i] + node.getA();
-				int ny = dy[i] + node.getB();
+				int nx = node.getA() + dx[i];
+				int ny = node.getB() + dy[i];
 				
 				if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
 					continue;
 				}
 				
-				if(array[nx][ny] == 0) {
-					array[nx][ny] = 1;
-					q.offer(new Nodes7576(nx, ny, cnt+1));
+				if(graph[nx][ny] == 0) {
+					graph[nx][ny] = 1;
+					q.offer(new Node7576(nx, ny, cnt+1));
 				}
 			}
 		}
 		
-		return check() ? cnt : -1;
-	}
-	
-	static boolean check() {
-		
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<m; j++) {
-				if(array[i][j] == 0) {
-					return false;
+				if(graph[i][j] == 0) {
+					cnt = -1;
 				}
 			}
 		}
 		
-		return true;
+		return cnt;
 	}
 	
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		StringTokenizer inserts = new StringTokenizer(scanner.nextLine());
-		m = Integer.parseInt(inserts.nextToken());
-		n = Integer.parseInt(inserts.nextToken());
-		
-		array = new int[n][m];
-		
-		for(int i=0; i<n; i++) {
-			inserts = new StringTokenizer(scanner.nextLine());
-			
-			for(int j=0; j<m; j++) {
-				array[i][j] = Integer.parseInt(inserts.nextToken());
-			}
-		}
-		
-		System.out.println(bfs());
-	}
 }

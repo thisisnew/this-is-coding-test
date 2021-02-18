91355a6323c525e079ddc8d5af3afc2d8523d7ca
thisisnew <thisisnew@naver.com>
2021-02-18 09:35:37 +0900

    아스키코드 출력 문자열처리

---
+++ study-algorithm/src/baekjoon/string/Ex11654.java
@@ -0,0 +1,18 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Ex11654 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		
+		char c = input.charAt(0);
+		
+		System.out.println((int) c);
+		
+	}
+}

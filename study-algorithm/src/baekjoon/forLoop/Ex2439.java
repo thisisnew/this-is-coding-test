737a6239e415a615f07b124dea83846d72b5aa65
thisisnew <thisisnew@naver.com>
2021-02-19 20:12:10 +0900

    별찍기

---
+++ study-algorithm/src/baekjoon/forLoop/Ex2439.java
@@ -0,0 +1,30 @@
+package baekjoon.forLoop;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Ex2439 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=n-i; j>0; j--) {
+				bw.write(" ");
+			}
+			for(int k=0; k<i; k++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+		
+	}
+}

f742311ea457f9b5cce6c0031bbbcf89c672dbd4
thisisnew <thisisnew@naver.com>
2021-02-19 21:27:06 +0900

    평균은 넘겠지

--- study-algorithm/src/baekjoon/oneDimensionArray/Ex4344.java
+++ study-algorithm/src/baekjoon/oneDimensionArray/Ex4344.java
@@ -1,10 +1,8 @@
 package baekjoon.oneDimensionArray;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Ex4344 {

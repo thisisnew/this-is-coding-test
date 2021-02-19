1f87992aaff8fa3cdc8983a8292ad22aec2844e9
thisisnew <thisisnew@naver.com>
2021-02-19 20:08:09 +0900

    A+B

---
+++ study-algorithm/src/baekjoon/forLoop/Ex11022.java
@@ -0,0 +1,30 @@
+package baekjoon.forLoop;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Ex11022 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b =  Integer.parseInt(st.nextToken());
+			
+			bw.write("Case #" + (i+1) + ": " + a +" + "+ b + " = " + (a+b) +"\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}

1ad32808c6f3083c19bff1efd826fa7b96b89c74
thisisnew <thisisnew@naver.com>
2021-02-19 20:50:28 +0900

    평균

---
+++ study-algorithm/src/baekjoon/oneDimensionArray/Ex1546.java
@@ -0,0 +1,37 @@
+package baekjoon.oneDimensionArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex1546 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int max = arr[n-1];
+		
+		double avg = 0;
+		double sum = 0;
+		
+		for(int i: arr) {
+			sum += (double) i / max * 100;
+		}
+		
+		avg = sum/n;
+		
+		System.out.println(avg);
+	}
+}

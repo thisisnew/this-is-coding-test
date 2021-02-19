822fb853f73f76fdfca29d2f81f9633e70fe03d3
thisisnew <thisisnew@naver.com>
2021-02-19 19:50:19 +0900

    합 구하기

---
+++ study-algorithm/src/baekjoon/forLoop/Ex8393.java
@@ -0,0 +1,20 @@
+package baekjoon.forLoop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex8393 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int sum = 0;
+		
+		for(int i=1; i<=n; i++) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
+	}
+}

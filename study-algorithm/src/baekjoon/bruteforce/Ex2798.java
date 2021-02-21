93d895ce447c886f63180bda4c5c2c56606d3cbd
thisisnew <thisisnew@naver.com>
2021-02-21 17:20:25 +0900

    블랙잭

--- study-algorithm/src/baekjoon/bruteforce/Ex2798.java
+++ study-algorithm/src/baekjoon/bruteforce/Ex2798.java
@@ -34,7 +34,7 @@ public class Ex2798 {
 		int answer = 0;
 		
 		for(int i=0; i<n-2; i++) {
-			for(int j=i+1; j<n-2; j++) {
+			for(int j=i+1; j<n-1; j++) {
 				for(int k=j+1; k<n; k++) {
 					int total = arr[i] + arr[j] + arr[k];
 					

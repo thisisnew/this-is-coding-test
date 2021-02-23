1afceb5892e33ebf4833e0017de0a769b2bb00bf
thisisnew <thisisnew@naver.com>
2021-02-23 10:46:00 +0900

    바이러스

--- study-algorithm/src/baekjoon/bfsdfs/Ex2606.java
+++ study-algorithm/src/baekjoon/bfsdfs/Ex2606.java
@@ -3,52 +3,47 @@ package baekjoon.bfsdfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Ex2606 {
 	
-	static int n, m, answer;
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+	static int m, n;
+	static int[][] graph;
 	static boolean[] isVisited;
+	static int answer;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		
+		graph = new int[n+1][n+1];
+		isVisited = new boolean[n+1];
+		
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			graph[a][b] = graph[b][a] = 1;
+		}
+		
+		dfs(1);
+		
+		System.out.println(answer);
+	}
 	
 	static void dfs(int x) {
 		isVisited[x] = true;
 		
-		for(int i=0; i<graph.get(x).size(); i++) {
-			int n = graph.get(x).get(i);
-			
-			if(!isVisited[n]) {
+		for(int i=1; i<=n; i++) {
+			if(!isVisited[i] && graph[x][i] == 1) {
 				answer++;
-				dfs(n);
+				isVisited[i] = true;
+				dfs(i);
 			}
 		}
-	}
-	
-	public static void main(String[] args) throws IOException {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		m = Integer.parseInt(br.readLine());
-		
-		isVisited = new boolean[n+1];
-		
-		for(int i=0; i<=n; i++) {
-			graph.add(new ArrayList<Integer>());
-		}
-		
-		for(int i=0; i<m; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			graph.get(a).add(b);
-			graph.get(b).add(a);
-		}
-		
-		answer = 0;
-		dfs(1);
-		
-		System.out.println(answer);
 	}
 }

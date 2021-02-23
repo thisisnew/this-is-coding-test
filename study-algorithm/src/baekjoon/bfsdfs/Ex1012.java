067d836e31e9758bd8674b840313a10ba3aa2ab5
thisisnew <thisisnew@naver.com>
2021-02-23 11:22:55 +0900

    유기농배추

--- study-algorithm/src/baekjoon/bfsdfs/Ex1012.java
+++ study-algorithm/src/baekjoon/bfsdfs/Ex1012.java
@@ -7,33 +7,30 @@ import java.util.StringTokenizer;
 
 public class Ex1012 {
 	
-	static int t, m , n, k;
+	static int m, n, k, t;
 	static int[][] graph;
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		t = Integer.parseInt(br.readLine());
-		int cnt = 0;
 		
-		while(cnt != t) {
-			st = new StringTokenizer(br.readLine());
+		for(int r=0; r<t; r++) {
 			
+			int answer = 0;
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			m = Integer.parseInt(st.nextToken());
 			n = Integer.parseInt(st.nextToken());
 			k = Integer.parseInt(st.nextToken());
 			
-			graph = new int[n][m];
+			graph = new int[n+1][m+1];
 			
 			for(int i=0; i<k; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				
 				graph[b][a] = 1;
 			}
-			
-			int answer = 0;
 			
 			for(int i=0; i<n; i++) {
 				for(int j=0; j<m; j++) {
@@ -44,13 +41,12 @@ public class Ex1012 {
 			}
 			
 			System.out.println(answer);
-			cnt++;
 		}
 	}
 	
 	static boolean dfs(int x, int y) {
 		
-		if(x < 0 || x >= n || y < 0 || y >= m) {
+		if(x < 0 || x > n || y < 0 || y > m) {
 			return false;
 		}
 		
@@ -67,5 +63,4 @@ public class Ex1012 {
 		
 		return false;
 	}
-	
 }

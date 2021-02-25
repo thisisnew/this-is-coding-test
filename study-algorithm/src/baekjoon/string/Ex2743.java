30253174a6209ee0630203be55ed6fab89345a80
thisisnew <thisisnew@naver.com>
2021-02-26 00:51:44 +0900

    단어길이재기

---
+++ study-algorithm/src/baekjoon/string/Ex2743.java
@@ -0,0 +1,13 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex2743 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		System.out.println(input.length());
+	}
+}

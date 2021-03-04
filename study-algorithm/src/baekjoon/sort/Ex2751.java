cd65586d8db4d36862abc21861cf0b8cc38fd606
thisisnew <thisisnew@naver.com>
2021-03-04 23:08:16 +0900

    수 정렬하기 2

---
+++ study-algorithm/src/baekjoon/sort/Ex2751.java
@@ -0,0 +1,31 @@
+package baekjoon.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Ex2751 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<Integer>();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<n; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(list);
+		
+		for(int value : list) {
+			sb.append(value).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+}

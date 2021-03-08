4ea45339b3390bc96cceee1b5e252a69597bd509
thisisnew <thisisnew@naver.com>
2021-03-08 11:50:12 +0900

    알파벳 거리

---
+++ study-algorithm/src/baekjoon/string/Ex5218.java
@@ -0,0 +1,49 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex5218 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i=65; i<=90; i++) {
+			
+		}
+		
+		for(int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			String input = st.nextToken();
+			String compInput = st.nextToken();
+			
+			int[] array = new int[input.length()];
+			
+			for(int j=0; j<input.length(); j++) {
+				int x = (int) input.charAt(j) - 64;
+				int y = (int) compInput.charAt(j) - 64;
+				
+				if(y >= x) {
+					array[j] = y-x;
+				} 
+				
+				if(y < x) {
+					array[j] = (y+26) - x;
+				}
+			}
+			
+			String answer = "Distances: ";
+			
+			for(int val : array) {
+				answer += val + " ";
+			}
+			
+			System.out.println(answer);
+			
+		}
+	}
+}

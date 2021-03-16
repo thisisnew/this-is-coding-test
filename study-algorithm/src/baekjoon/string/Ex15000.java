1909c6e028b56647a00f796d326aaced8bd2c5da
thisisnew <thisisnew@naver.com>
2021-03-16 16:08:23 +0900

    CAPS

---
+++ study-algorithm/src/baekjoon/string/Ex15000.java
@@ -0,0 +1,15 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex15000 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = br.readLine();
+		
+		System.out.println(input.toUpperCase());
+	}
+}

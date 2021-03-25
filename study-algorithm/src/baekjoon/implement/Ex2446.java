f6f08cb420fd20561fce5f47ff698b54030465ad
thisisnew <thisisnew@naver.com>
2021-03-25 16:26:10 +0900

    별 찍기 - 9

---
+++ study-algorithm/src/baekjoon/implement/Ex2446.java
@@ -0,0 +1,40 @@
+package baekjoon.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex2446 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int total = 2 * n -1;
+		int star = 0;
+		int space = 0;
+		int cnt = n+1;
+		
+		for(int i=1; i<=total; i++) {
+			
+			if(i <= n) {
+				cnt--;
+			} else {
+				cnt++;
+			}
+			
+			star = 2 * cnt -1;
+			space = (total - star)/2;
+			
+			for(int j=0; j<space; j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=0; j<star; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			
+		}
+	}
+}

cf9d2e2881e842479e7f4ecfb013a952eb61243e
thisisnew <thisisnew@naver.com>
2021-04-01 18:13:44 +0900

    뉴비의 기준은 뭘까?

---
+++ study-algorithm/src/baekjoon/implement/Ex19944.java
@@ -0,0 +1,27 @@
+package baekjoon.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex19944 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		if(m <= 2) {
+			System.out.println("NEWBIE!");
+		} else if(m > 2 && m <= n){
+			System.out.println("OLDBIE!");
+		} else {
+			System.out.println("TLE!");
+		}
+		
+	}
+}

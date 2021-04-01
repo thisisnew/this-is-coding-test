d2c30cda547f80b681cabece7900a84b6ccbc872
thisisnew <thisisnew@naver.com>
2021-04-02 00:33:47 +0900

    검증수

---
+++ study-algorithm/src/baekjoon/implement/Ex2475.java
@@ -0,0 +1,25 @@
+package baekjoon.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex2475 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int sum = 0;
+		
+		while(st.hasMoreTokens()) {
+			int n = Integer.parseInt(st.nextToken());
+			
+			sum += Math.pow(n, 2);
+			
+		}
+		
+		System.out.println(sum % 10);
+	}
+}

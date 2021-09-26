82e16f1d9f93d270cfa9e1dab7a96aebc2e34181
thisisnew <thisisnew@naver.com>
2021-09-26 17:01:14 +0900

    백준 알고리즘공부. 아!

---
+++ study-algorithm/src/baekjoon/string/Ex4999.java
@@ -0,0 +1,20 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex4999 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String jh = br.readLine();
+		String dr = br.readLine();
+		
+		if(jh.length() < dr.length()) {
+			System.out.println("no");
+		} else {
+			System.out.println("go");
+		}
+	}
+}

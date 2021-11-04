9a0aa0de1e709770b6cfc7b57d58ef3732bcb0ce
thisisnew <thisisnew@naver.com>
2021-11-05 00:11:51 +0900

    숫자놀이 해결

--- study-algorithm/src/baekjoon/string/Ex1755.java
+++ study-algorithm/src/baekjoon/string/Ex1755.java
@@ -4,13 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Ex1755 {
-	final static String[] ARRAY = new String[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-			"nine" };
+	final static String[] STRING_ARRAY = new String[] { "zero", "one", "two", "three", "four", "five", "six", "seven",
+			"eight", "nine" };
+
+	final static int[] INT_ARRAY = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 	public static void main(String[] args) throws IOException {
 		List<String> result = new ArrayList<String>();
@@ -24,24 +27,33 @@ public class Ex1755 {
 			String num = String.valueOf(i);
 			StringBuffer word = new StringBuffer();
 			for (int j = 0; j < num.length(); j++) {
-				int idx =  num.charAt(j) - '0';
-				String s = ARRAY[idx];
+				int idx = num.charAt(j) - '0';
+				String s = STRING_ARRAY[idx];
 				word.append(s).append(" ");
 			}
 			result.add(word.toString().trim());
 		}
-		
+
 		Collections.sort(result);
-		
-		int cnt = 1;
-		for(String r : result) {
-			if (cnt == 10 ){
+
+		int cnt = 0;
+		for (String r : result) {
+			if (cnt == 10) {
 				System.out.println();
 				cnt = 0;
 			}
-			System.out.print(r.length()+" ");
+			
+			String res = "";
+			StringTokenizer st2 = new StringTokenizer(r);
+			while(st2.hasMoreTokens()) {
+				String i = st2.nextToken();
+				int w = Arrays.asList(STRING_ARRAY).indexOf(i);
+				res += String.valueOf(w);	
+			}
+		
+			System.out.print(res + " ");
 			cnt++;
 		}
-		
+
 	}
 }

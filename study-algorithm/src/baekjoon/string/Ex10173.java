654be681db077b8e1128149162e3948b1eb85642
thisisnew <thisisnew@naver.com>
2021-11-14 21:44:30 +0900

    니모를 찾아서

---
+++ study-algorithm/src/baekjoon/string/Ex10173.java
@@ -0,0 +1,29 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex10173 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			String input = br.readLine();
+
+			if (input.equals("EOI")) {
+				break;
+			}
+
+			input = input.toUpperCase();
+
+			if (input.contains("NEMO")) {
+				System.out.println("Found");
+			} else {
+				System.out.println("Missing");
+			}
+
+		}
+	}
+}

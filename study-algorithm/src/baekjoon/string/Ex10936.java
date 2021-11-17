2a7084c0031115afa562923caf047b70f4ea11b1
thisisnew <thisisnew@naver.com>
2021-11-17 22:39:09 +0900

    base64 디코딩

---
+++ study-algorithm/src/baekjoon/string/Ex10936.java
@@ -0,0 +1,19 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Base64;
+
+public class Ex10936 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+		byte[] inputBytes = Base64.getDecoder().decode(input);
+		
+		System.out.println(new String(inputBytes,"UTF-8"));
+	}
+}

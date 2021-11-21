39e4002fa485484f2b27c161f1e4e0ce032aa856
thisisnew <thisisnew@naver.com>
2021-11-21 13:29:10 +0900

    도깨비말 풀이중

--- study-algorithm/src/baekjoon/string/Ex9226.java
+++ study-algorithm/src/baekjoon/string/Ex9226.java
@@ -3,43 +3,52 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Ex9226 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		char[] cArr = { 'a', 'e', 'i', 'o', 'u' };
-
 		while (true) {
 			String input = br.readLine();
+			String result = input;
 
 			if (input.equals("#")) {
 				break;
 			}
 
-			Queue<Character> q = new LinkedList<Character>();
-
-			for (int i = 0; i < input.length(); i++) {
-				q.add(input.charAt(i));
-			}
-
-			while (true) {
-				char c = q.poll();
-				switch (c) {
-				case 'a':
-				case 'e':
-				case 'i':
-				case 'o':
-				case 'u':
+			if (!input.contains("a") && !input.contains("e") && !input.contains("i") && !input.contains("o") && !input.contains("u")) {
+				System.out.println(result + "ay");
+				continue;
+			} else {
+				for (int i = 0; i < input.length(); i++) {
+					char c = input.charAt(i);
+					
+					switch (c) {
+					case 'a': {
+						break;
+					}
+					case 'e': {
+						break;
+					}
+					case 'i': {
+						break;
+					}
+					case 'o': {
+						break;
+					}
+					case 'u': {
+						break;
+					}
+					default: {
+						result = input.substring(i + 1) + input.substring(0, i + 1);
+						break;
+					}
+					}
+					
+					System.out.println(result + "ay");
 					break;
-				}
-				q.add(c);
+				}				
 			}
-			
-			//String result = q.toString();
-			//System.out.println(result + "ay");
 		}
 	}
 }

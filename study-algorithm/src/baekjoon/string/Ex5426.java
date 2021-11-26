90527f7dc821c85971769ff7737ffda6a45559dd
thisisnew <thisisnew@naver.com>
2021-11-26 18:51:09 +0900

    비밀편지 풀이중

---
+++ study-algorithm/src/baekjoon/string/Ex5426.java
@@ -0,0 +1,23 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+
+public class Ex5426 {
+
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			String input = br.readLine();
+
+			
+		}
+
+	}
+
+}

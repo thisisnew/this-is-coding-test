2828c2b56b7bf551087ed82cf7d093e5c03fc147
thisisnew <thisisnew@naver.com>
2021-12-06 21:16:58 +0900

    홍익 댄스파티 풀이중

---
+++ study-algorithm/src/baekjoon/string/Ex23321.java
@@ -0,0 +1,18 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+
+public class Ex23321 {
+
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for (int i = 0; i < 5; i++) {
+			String input = br.readLine();
+		}
+
+	}
+
+}

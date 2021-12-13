f77b7b6190c97e54f67dd2aec74e3fe1d4672b0c
thisisnew <thisisnew@naver.com>
2021-12-13 21:52:45 +0900

    회전 풀이중

---
+++ study-algorithm/src/baekjoon/string/Ex23813.java
@@ -0,0 +1,43 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+
+public class Ex23813 {
+
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+		int total = 0;
+
+		while (true) {
+			String turnInput = turnInput(input);
+
+			total += Integer.parseInt(turnInput);
+
+			if (turnInput.equals(input)) {
+				break;
+			}
+		}
+
+		System.out.println(total);
+	}
+
+	private static String turnInput(String input) {
+
+		char[] arr = new char[input.length()];
+
+		for (int i = 0; i < input.length(); i++) {
+			arr[i] = input.charAt(i);
+		}
+
+		char c = arr[arr.length - 1];
+		arr[arr.length - 1] = arr[0];
+		arr[0] = c;
+
+		return new String(arr);
+	}
+}

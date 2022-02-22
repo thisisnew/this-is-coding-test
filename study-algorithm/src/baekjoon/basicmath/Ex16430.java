78f4e6df40173649ceae77fd41436dd0f7153cca
thisisnew <thisisnew@naver.com>
2022-02-22 12:37:59 +0900

    제리와 톰

---
+++ study-algorithm/src/baekjoon/basicmath/Ex16430.java
@@ -0,0 +1,19 @@
+package baekjoon.basicmath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex16430 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		int remain = B - A;
+		System.out.println(remain + " " + B);
+	}
+}

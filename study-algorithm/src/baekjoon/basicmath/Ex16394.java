a6b7110ae94f48fc73acc814195adc1c0c322785
thisisnew <thisisnew@naver.com>
2022-02-22 12:33:26 +0900

    홍익대학교

---
+++ study-algorithm/src/baekjoon/basicmath/Ex16394.java
@@ -0,0 +1,17 @@
+package baekjoon.basicmath;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ex16394 {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		System.out.println(input - 1946);
+			
+	}
+}

4f5653d51862ded7f2d43b51f93fe67fb966f4c1
thisisnew <thisisnew@naver.com>
2022-03-02 21:03:19 +0900

    얼마?

---
+++ study-algorithm/src/baekjoon/basicmath/Ex9325.java
@@ -0,0 +1,33 @@
+package baekjoon.basicmath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex9325 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int test = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < test; i++) {
+
+			int s = Integer.parseInt(br.readLine());
+
+			int n = Integer.parseInt(br.readLine());
+
+			for (int j = 0; j < n; j++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int q = Integer.parseInt(st.nextToken());
+				int p = Integer.parseInt(st.nextToken());
+
+				s += (q * p);
+			}
+
+			System.out.println(s);
+
+		}
+
+	}
+}

4ddbf482174b531b108afdcec18f953cd844b2c1
thisisnew <thisisnew@naver.com>
2022-03-03 22:21:52 +0900

    고려대는 사랑입니다

---
+++ study-algorithm/src/baekjoon/function/Ex11942.java
@@ -0,0 +1,9 @@
+package baekjoon.function;
+
+import java.io.IOException;
+
+public class Ex11942 {
+	public static void main(String[] args) throws IOException {
+		System.out.println("고려대학교");
+	}
+}

257789dc3478dc7f5a71131fc553450c4af8f81f
thisisnew <thisisnew@naver.com>
2022-03-03 22:30:33 +0900

    별 찍기 - 17

---
+++ study-algorithm/src/baekjoon/function/Ex10992.java
@@ -0,0 +1,47 @@
+package baekjoon.function;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex10992 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int stars = 1;
+
+		for (int i = 0; i < n; i++) {
+			if (i == n - 1) {
+				drawStarsAll(stars);
+			} else {
+				drawStarsStartAndEnd(stars);
+			}
+		}
+	}
+
+	static void drawStarsStartAndEnd(int stars) {
+
+		int n = stars * 2 - 1;
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("*");
+		}
+
+	}
+
+	static void drawStarsAll(int stars) {
+
+		int n = stars * 2 - 1;
+
+		for (int i = 0; i < n; i++) {
+			if (i == 0 || i == n - 1) {
+				System.out.print("*");
+			} else {
+				System.out.print(" ");
+			}
+		}
+
+	}
+
+}

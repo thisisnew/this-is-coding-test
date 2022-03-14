146255ecaf3b8247891988916e840e1c51448c48
thisisnew <thisisnew@naver.com>
2022-03-14 22:06:53 +0900

    8진수, 10진수, 16진수

---
+++ study-algorithm/src/baekjoon/string/Ex11816.java
@@ -0,0 +1,39 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex11816 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+		
+		char i0 = input.charAt(0);
+		char i1 = input.charAt(1);
+		
+		String answer = input;
+		
+		if(i0 == '0') {
+			
+			if(i1 == 'X') {
+				answer = calculateHexadecimalToDeciaml(answer);
+			} else {
+				answer = calculateHexadecimalToDeciaml(answer);
+			}
+			
+		} 
+		
+		System.out.println(answer);
+	}
+	
+	
+	static String calculateOctalToDecimal(String input) {
+		return "";
+	}
+	
+	static String calculateHexadecimalToDeciaml(String input) {
+		return "";	
+	}
+}

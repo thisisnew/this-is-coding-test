d806dfdb04bfe86a8119dd531583741fee2f6db9
thisisnew <thisisnew@naver.com>
2022-03-14 21:58:09 +0900

    별 찍기 - 17

--- study-algorithm/src/baekjoon/function/Ex10992.java
+++ study-algorithm/src/baekjoon/function/Ex10992.java
@@ -10,24 +10,27 @@ public class Ex10992 {
 
 		int n = Integer.parseInt(br.readLine());
 
-		 for(int i=1; i<n; i++) {
-	        	System.out.printf(" ");
-	        }System.out.println("*");
-	        
-	        for(int i=2; i<=n-1; i++) {
-	        	for(int j=1; j<=n-i; j++) {
-	        		System.out.printf(" ");
-	        	} System.out.printf("*");
-	        	for(int j=1; j<=(i-1)*2-1; j++) {
-	        		System.out.printf(" ");
-	        	}System.out.println("*");
-	        	
-	        }
-	        
-	        if(n!=1) {
-				for(int z=1; z<2*n; z++) {
-					System.out.printf("*");
-				}
+		for (int i = 1; i < n; i++) {
+			System.out.printf(" ");
+		}
+		System.out.println("*");
+
+		for (int i = 2; i <= n - 1; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.printf(" ");
 			}
+			System.out.printf("*");
+			for (int j = 1; j <= (i - 1) * 2 - 1; j++) {
+				System.out.printf(" ");
+			}
+			System.out.println("*");
+
+		}
+
+		if (n != 1) {
+			for (int z = 1; z < 2 * n; z++) {
+				System.out.printf("*");
+			}
+		}
 	}
 }

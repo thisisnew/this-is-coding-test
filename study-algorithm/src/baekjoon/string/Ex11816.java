cf578c995d1a46bc1fce9cb92e56387680d9a62c
thisisnew <thisisnew@naver.com>
2022-03-15 20:27:21 +0900

    8진수, 10진수, 16진수

--- study-algorithm/src/baekjoon/string/Ex11816.java
+++ study-algorithm/src/baekjoon/string/Ex11816.java
@@ -29,10 +29,10 @@ public class Ex11816 {
 	}
 
 	static String calculateOctalToDecimal(String input) {
-		return "";
+		return String.valueOf(Integer.valueOf(Integer.parseInt(input.substring(1),8)));
 	}
 
 	static String calculateHexadecimalToDeciaml(String input) {
-		return "";
+		return String.valueOf(Integer.valueOf(Integer.parseInt(input.substring(2),16)));
 	}
 }

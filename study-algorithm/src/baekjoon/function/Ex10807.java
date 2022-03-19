d0eaba14adfe5b2438d36008a8c09ac6536aa098
thisisnew <thisisnew@naver.com>
2022-03-19 22:45:27 +0900

    개수 세기

---
+++ study-algorithm/src/baekjoon/function/Ex10807.java
@@ -0,0 +1,12 @@
+package baekjoon.function;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex10807 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+	}
+}

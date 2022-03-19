8546ff403dfae3157022359627246a9635f92bab
thisisnew <thisisnew@naver.com>
2022-03-19 22:52:30 +0900

    개수 세기

--- study-algorithm/src/baekjoon/function/Ex10807.java
+++ study-algorithm/src/baekjoon/function/Ex10807.java
@@ -3,8 +3,6 @@ package baekjoon.function;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Ex10807 {
@@ -13,22 +11,19 @@ public class Ex10807 {
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int v = Integer.parseInt(br.readLine());
-		
-		Map<Integer, Integer> numMap = new HashMap<Integer, Integer>();
-		
-		for(int i=0; i<n; i++) {
+
+		int result = 0;
+
+		for (int i = 0; i < n; i++) {
 			int input = Integer.parseInt(st.nextToken());
-			
-			if(!numMap.containsKey(input)) {
-				numMap.put(input, 1);
-			} else {
-				int count = numMap.get(input);
-				numMap.put(input, count + 1);
+
+			if (input != v) {
+				continue;
 			}
-	
+
+			result++;
 		}
-		
-		int result = numMap.get(v);
-		System.out.println(result > 0 ? result : 0 );
+
+		System.out.println(result);
 	}
 }

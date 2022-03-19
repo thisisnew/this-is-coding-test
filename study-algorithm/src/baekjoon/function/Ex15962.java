7b747217c3aac7dc4e6bdf4344a92a2926896424
thisisnew <thisisnew@naver.com>
2022-03-19 22:43:47 +0900

    새로운 시작

---
+++ study-algorithm/src/baekjoon/function/Ex15962.java
@@ -0,0 +1,9 @@
+package baekjoon.function;
+
+import java.io.IOException;
+
+public class Ex15962 {
+	public static void main(String[] args) throws IOException {
+		System.out.println("ÆÄÀÌÆÃ!!");
+	}
+}

41c29091a3b76340a6178307e3c8a613bfe97fee
thisisnew <thisisnew@naver.com>
2022-03-20 22:10:06 +0900

    와이버스 부릉부릉

---
+++ study-algorithm/src/baekjoon/function/Ex14645.java
@@ -0,0 +1,23 @@
+package baekjoon.function;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex14645 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < n; i++) {
+			 st = new StringTokenizer(br.readLine());
+			 int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.println("ºñ¿ÍÀÌ");
+	}
+}

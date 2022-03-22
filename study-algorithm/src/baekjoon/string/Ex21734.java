5904504a3341268838cfbee0af6d858be75b98d8
thisisnew <thisisnew@naver.com>
2022-03-22 20:45:44 +0900

    SMUPC의 등장

--- study-algorithm/src/baekjoon/string/Ex21734.java
+++ study-algorithm/src/baekjoon/string/Ex21734.java
@@ -13,38 +13,37 @@ public class Ex21734 {
 		print(input);
 	}
 
-	
 	private static void print(String input) {
-		
-		for(int i=0; i<input.length(); i++) {
-			int num = (int)input.charAt(i) - 64;
-			
+
+		for (int i = 0; i < input.length(); i++) {
+			int num = (int) input.charAt(i) - 64;
+
 			System.out.println(makeString(makeCount(num), input.charAt(i)));
 		}
-		
+
 	}
-	
+
 	private static int makeCount(int num) {
-		
+
 		String n = String.valueOf(num);
-		
+
 		int cnt = 0;
-		
-		for(int i=0; i<n.length(); i++) {
-			cnt+= Character.getNumericValue(n.charAt(i));
+
+		for (int i = 0; i < n.length(); i++) {
+			cnt += Character.getNumericValue(n.charAt(i));
 		}
-		
+
 		return cnt;
 	}
-	
+
 	private static String makeString(int cnt, char c) {
-		
+
 		StringBuffer sb = new StringBuffer();
-		
-		for(int i=0; i<cnt; i++) {
+
+		for (int i = 0; i < cnt; i++) {
 			sb.append(c);
 		}
-		
+
 		return sb.toString();
 	}
 }

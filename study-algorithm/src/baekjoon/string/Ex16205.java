1cfa28b6647af3ba9177e9d46dca7e56927da025
thisisnew <thisisnew@naver.com>
2022-03-23 21:21:39 +0900

    변수명

--- study-algorithm/src/baekjoon/string/Ex16205.java
+++ study-algorithm/src/baekjoon/string/Ex16205.java
@@ -17,9 +17,9 @@ public class Ex16205 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		String input = st.nextToken();
-		
-		String word = splitWord(input,n);
-		
+
+		String word = splitWord(input, n);
+
 		printCamelCase(word);
 		printSnakeCase(word);
 		printPascalCase(word);
@@ -30,43 +30,43 @@ public class Ex16205 {
 		String word = "";
 
 		switch (n) {
-			case CAMEL_CASE: {
-	
-				for (int i = 0; i < input.length(); i++) {
-	
-					char c = input.charAt(i);
-	
-					if (c > 'a') {
-						word += "_";
-					}
-	
-					word += c;
+		case CAMEL_CASE: {
+
+			for (int i = 0; i < input.length(); i++) {
+
+				char c = input.charAt(i);
+
+				if (c >= 'A' && c <= 'Z') {
+					word += "_";
 				}
-	
-				break;
+
+				word += c;
 			}
-			
-			case SNAKE_CASE: {
-				word = input;
-	
-				break;
-			}
-			
-			case PASCAL_CASE: {
-	
-				for (int i = 0; i < input.length(); i++) {
-	
-					char c = input.charAt(i);
-	
-					if (i > 0 && c > 'a') {
-						word += "_";
-					}
-	
-					word += c;
+
+			break;
+		}
+
+		case SNAKE_CASE: {
+			word = input;
+
+			break;
+		}
+
+		case PASCAL_CASE: {
+
+			for (int i = 0; i < input.length(); i++) {
+
+				char c = input.charAt(i);
+
+				if (i > 0 && c >= 'A' && c <= 'Z') {
+					word += "_";
 				}
-	
-				break;
+
+				word += c;
 			}
+
+			break;
+		}
 		}
 
 		return word.toLowerCase();
@@ -76,33 +76,40 @@ public class Ex16205 {
 
 		String camel = "";
 		String[] words = word.split("_");
-		
-		for(int i = 0; i<words.length; i++) {
+
+		for (int i = 0; i < words.length; i++) {
 			String wordProp = words[i];
-			
-			if(i == 0) {
+
+			if (i == 0) {
 				camel += wordProp;
 				continue;
 			}
-			
+
 			String first = wordProp.substring(0, 1).toUpperCase();
 			String remain = wordProp.substring(1);
-			
-			camel += (first + remain);			
+
+			camel += (first + remain);
 		}
-		
+
 		System.out.println(camel);
 	}
 
 	private static void printSnakeCase(String word) {
-
-		
-
+		System.out.println(word);
 	}
 
 	private static void printPascalCase(String word) {
+		String pascal = "";
+		String[] words = word.split("_");
 
-		
+		for (int i = 0; i < words.length; i++) {
+			String wordProp = words[i];
+			String first = wordProp.substring(0, 1).toUpperCase();
+			String remain = wordProp.substring(1);
 
+			pascal += (first + remain);
+		}
+
+		System.out.println(pascal);
 	}
 }

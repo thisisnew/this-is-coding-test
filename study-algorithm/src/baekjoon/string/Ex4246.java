632a83789672277ddbb95a7c0bfb014c029ce149
thisisnew <thisisnew@naver.com>
2022-03-24 21:51:24 +0900

    To and Fro

--- study-algorithm/src/baekjoon/string/Ex4246.java
+++ study-algorithm/src/baekjoon/string/Ex4246.java
@@ -9,74 +9,74 @@ public class Ex4246 {
 	static int n = 0;
 	static int len = 0;
 	static String input;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		while(true) {
+
+		while (true) {
 			n = Integer.parseInt(br.readLine());
-			
-			if(n == 0) {
+
+			if (n == 0) {
 				break;
 			}
-			
+
 			input = br.readLine();
-			len = input.length()/n;
-			
+			len = input.length() / n;
+
 			char[][] arr = makeArr(input);
-			
+
 			print(arr);
 		}
 	}
-	
-	private static char[][] makeArr(String input){
-		
+
+	private static char[][] makeArr(String input) {
+
 		char[][] arr = new char[len][n];
 		boolean isReverse = false;
 		int lat = 0;
 		int lon = 0;
-		
-		for(int i=0; i<input.length(); i++) {
-			
-			char c = input.charAt(i); 
-			
-			if(isReverse) {
-				arr[lon][n - lat -1] = c;
+
+		for (int i = 0; i < input.length(); i++) {
+
+			char c = input.charAt(i);
+
+			if (isReverse) {
+				arr[lon][n - lat - 1] = c;
 			} else {
-				arr[lon][lat] = c; 
+				arr[lon][lat] = c;
 			}
-			
+
 			lat++;
-			
-			if(lat == n) {
+
+			if (lat == n) {
 				isReverse = !isReverse;
 				lon++;
 				lat = 0;
 			}
 		}
-		
+
 		return arr;
 	}
-	
+
 	private static void print(char[][] arr) {
-		
+
 		String result = "";
 		int lat = 0;
 		int lon = 0;
-		
-		for(int i=0; i<input.length(); i++) { 
-			
-			result += arr[lon][lat]; 
-			
+
+		for (int i = 0; i < input.length(); i++) {
+
+			result += arr[lon][lat];
+
 			lon++;
-			
-			if(lon == len) {
+
+			if (lon == len) {
 				lat++;
 				lon = 0;
 			}
-			
+
 		}
-		
+
 		System.out.println(result);
 	}
 }

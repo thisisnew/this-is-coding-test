57bb9b7de071cf604fdf51ac5bd13569cf073023
thisisnew <thisisnew@naver.com>
2022-03-25 19:58:26 +0900

    Password check

--- study-algorithm/src/baekjoon/string/Ex15239.java
+++ study-algorithm/src/baekjoon/string/Ex15239.java
@@ -8,93 +8,93 @@ public class Ex15239 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
+
 		int n = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			int len = Integer.parseInt(br.readLine());
 			String pass = br.readLine();
-			
-			if(!isValidLength(len)) {
+
+			if (!isValidLength(len)) {
 				System.out.println("invalid");
 				continue;
 			}
-			
-			if(!hasMoreThenOneDigit(pass)) {
+
+			if (!hasMoreThenOneDigit(pass)) {
 				System.out.println("invalid");
 				continue;
 			}
-			
-			if(!hasMoreThenOneUpperCase(pass)) {
+
+			if (!hasMoreThenOneUpperCase(pass)) {
 				System.out.println("invalid");
 				continue;
 			}
-			
-			if(!hasMoreThenOneLowerCase(pass)) {
+
+			if (!hasMoreThenOneLowerCase(pass)) {
 				System.out.println("invalid");
 				continue;
 			}
-			
-			if(!hasMoreThenOneSymbol(pass)) {
+
+			if (!hasMoreThenOneSymbol(pass)) {
 				System.out.println("invalid");
 				continue;
 			}
-			
+
 			System.out.println("valid");
 		}
-		
+
 	}
-	
+
 	private static boolean isValidLength(int len) {
-		
-		if(len >= 12) {
+
+		if (len >= 12) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	private static boolean hasMoreThenOneDigit(String pass) {
-		
-		for(int i=0; i<pass.length(); i++) {
-			if(pass.charAt(i) >= '0' && pass.charAt(i) <= '9') {
+
+		for (int i = 0; i < pass.length(); i++) {
+			if (pass.charAt(i) >= '0' && pass.charAt(i) <= '9') {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	private static boolean hasMoreThenOneUpperCase(String pass) {
-		
-		for(int i=0; i<pass.length(); i++) {
-			if(pass.charAt(i) >= 'A' && pass.charAt(i) <= 'Z') {
+
+		for (int i = 0; i < pass.length(); i++) {
+			if (pass.charAt(i) >= 'A' && pass.charAt(i) <= 'Z') {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	private static boolean hasMoreThenOneLowerCase(String pass) {
-		
-		for(int i=0; i<pass.length(); i++) {
-			if(pass.charAt(i) >= 'a' && pass.charAt(i) <= 'z') {
+
+		for (int i = 0; i < pass.length(); i++) {
+			if (pass.charAt(i) >= 'a' && pass.charAt(i) <= 'z') {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	private static boolean hasMoreThenOneSymbol(String pass) {
-		
-		for(int i=0; i<pass.length(); i++) {
-			if("+_)(*&^%$#@!./,;{}".contains(String.valueOf(pass.charAt(i)))) {
+
+		for (int i = 0; i < pass.length(); i++) {
+			if ("+_)(*&^%$#@!./,;{}".contains(String.valueOf(pass.charAt(i)))) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
 }
